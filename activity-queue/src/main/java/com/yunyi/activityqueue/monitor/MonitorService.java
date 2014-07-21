83e55bb4eab9ab9b619b78c0eeb25449da4829f8
qizhou <qizhou@QIZHOU4.asiapacific.hpqcorp.net>
2014-07-21 13:06:11 +0800

    1: change to use UTF-8 encoding 2: close db connection

--- activity-queue/src/main/java/com/yunyi/activityqueue/monitor/MonitorService.java
+++ activity-queue/src/main/java/com/yunyi/activityqueue/monitor/MonitorService.java
@@ -6,70 +6,72 @@ import com.yunyi.activityqueue.db.ActivityQueueDAO;
 import com.yunyi.activityqueue.db.ActivityQueueDAOImpl;
 import com.yunyi.activityqueue.db.connectionmgr.ConnectionManager;
 import com.yunyi.activityqueue.reportor.RestfulReportor;
-
 import java.sql.Connection;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA. User: qizhou Date: 7/19/14 Time: 12:46 PM To
- * change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: qizhou Date: 7/19/14 Time: 12:46 PM To change this template use
+ * File | Settings | File Templates.
  */
 public class MonitorService implements Runnable {
 
-	private static MonitorService service = new MonitorService();
-	private ActivityQueueDAO activityQueueDao = new ActivityQueueDAOImpl();
+    private static MonitorService service = new MonitorService();
+    private ActivityQueueDAO activityQueueDao = new ActivityQueueDAOImpl();
 
-	private MonitorService() {
-	}
+    private MonitorService() {
+    }
 
-	public static MonitorService getInstance() {
-		return service;
-	}
+    public static MonitorService getInstance() {
+        return service;
+    }
 
-	public void startMonitor() {
-		Thread thd = new Thread(this);
-		thd.start();
-	}
+    public void startMonitor() {
+        Thread thd = new Thread(this);
+        thd.start();
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		System.out.println("Executing Monitor");
+        System.out.println("Executing Monitor");
 
-		while (!Thread.currentThread().isInterrupted()) {
-			System.out.println("Start processing ...");
-			try {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println("Start processing ...");
+            try {
 
-				// get connection for each scheduler call
-				Connection conn = ConnectionManager.getInstance().getConnection();
+                // get connection for each scheduler call
+                Connection conn = ConnectionManager.getInstance().getConnection();
 
-				// retrieve all activities
-				List<Activity> activities = activityQueueDao.getActivities(conn);
-				if (activities == null || activities.size() == 0) {
-					System.out.println("no record, end the job.");
-				}
-				for (Activity activity : activities) {
-					// report to restful server
-					System.out.println("report Activity...." + activity.getId());
-					RestfulReportor.getInstance().reportActivities(activity);
+                // retrieve all activities
+                List<Activity> activities = activityQueueDao.getActivities(conn);
+                if (activities == null || activities.size() == 0) {
+                    System.out.println("no record, end the job.");
+                }
+                for (Activity activity : activities) {
+                    // report to restful server
+                    System.out.println("report Activity...." + activity.getId());
+                    RestfulReportor.getInstance().reportActivities(activity);
 
-					activityQueueDao.deleteActivityByID(conn, activity.getId());
-				}
+                    activityQueueDao.deleteActivityByID(conn, activity.getId());
+                }
 
-				// conn.commit();
+                // close the connection.
+                conn.close();
 
-				System.out.println("Stop processing ...");
-				// now wai for a while
-				String interval = Configure.getInstance().get("interval");
-				int intervalMinute = Integer.valueOf(interval) * 60 * 1000;
+                // conn.commit();
 
-				Thread.sleep(intervalMinute);
+                System.out.println("Stop processing ...");
+                // now wai for a while
+                String interval = Configure.getInstance().get("interval");
+                int intervalMinute = Integer.valueOf(interval) * 60 * 1000;
 
-			} catch (Exception ex) {
-				ex.printStackTrace();
+                Thread.sleep(intervalMinute);
 
-			}
-		}
+            } catch (Exception ex) {
+                ex.printStackTrace();
 
-	}
+            }
+        }
+
+    }
 }
--- activity-queue/src/main/java/com/yunyi/activityqueue/reportor/RestfulReportor.java
+++ activity-queue/src/main/java/com/yunyi/activityqueue/reportor/RestfulReportor.java
@@ -2,136 +2,133 @@ package com.yunyi.activityqueue.reportor;
 
 import com.yunyi.activityqueue.bean.Activity;
 import com.yunyi.activityqueue.configure.Configure;
-
 import sun.misc.BASE64Encoder;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.json.JSONObject;
 
 /**
- * Created with IntelliJ IDEA. User: qizhou Date: 7/19/14 Time: 12:59 PM To
- * change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: qizhou Date: 7/19/14 Time: 12:59 PM To change this template use
+ * File | Settings | File Templates.
  */
 public class RestfulReportor {
 
-	private static RestfulReportor reportor = new RestfulReportor();
-	private String encodedAuthorization = null;
+    private static RestfulReportor reportor = new RestfulReportor();
+    private String encodedAuthorization = null;
 
-	private RestfulReportor() {
+    private RestfulReportor() {
 
-		BASE64Encoder enc = new sun.misc.BASE64Encoder();
-		String userName = Configure.getInstance().get("eXoUser");
-		String passwd = Configure.getInstance().get("eXoPasswd");
-		String userpassword = userName + ":" + passwd;
-		encodedAuthorization = enc.encode(userpassword.getBytes());
-	}
+        BASE64Encoder enc = new sun.misc.BASE64Encoder();
+        String userName = Configure.getInstance().get("eXoUser");
+        String passwd = Configure.getInstance().get("eXoPasswd");
+        String userpassword = userName + ":" + passwd;
+        encodedAuthorization = enc.encode(userpassword.getBytes());
+    }
 
-	public static RestfulReportor getInstance() {
-		return reportor;
-	}
+    public static RestfulReportor getInstance() {
+        return reportor;
+    }
 
-	// send restful request
-	public void reportActivities(Activity activity) throws Exception {
+    // send restful request
+    public void reportActivities(Activity activity) throws Exception {
 
-		HttpURLConnection httpConn = null;
-		OutputStream out = null;
+        HttpURLConnection httpConn = null;
+        OutputStream out = null;
 
-		try {
+        try {
 
-			String reportURL = Configure.getInstance().get("eXoServerUrl")  + "rest/private/activity/space";
+            String reportURL = Configure.getInstance().get("eXoServerUrl") + "rest/private/activity/space";
 
-			URL url = new URL(reportURL);
-			// System.out.println(Thread.currentThread().getName()+"-----------------begin to connect-------------------");
-			// long startTime = System.currentTimeMillis();
-			httpConn = (HttpURLConnection) url.openConnection();
-			// long endTime = System.currentTimeMillis();
-			// System.out.println(Thread.currentThread().getName()+"------------------Connected-----------------"+
-			// (endTime-startTime)/1000);
-			// httpConn.setRequestProperty("Content-Type",
-			// "application/xstream_xml");
-			httpConn.setRequestProperty("Content-Type", "application/json");
+            URL url = new URL(reportURL);
+            // System.out.println(Thread.currentThread().getName()+"-----------------begin to connect-------------------");
+            // long startTime = System.currentTimeMillis();
+            httpConn = (HttpURLConnection) url.openConnection();
+            // long endTime = System.currentTimeMillis();
+            // System.out.println(Thread.currentThread().getName()+"------------------Connected-----------------"+
+            // (endTime-startTime)/1000);
+            // httpConn.setRequestProperty("Content-Type",
+            // "application/xstream_xml");
+            httpConn.setRequestProperty("Content-Type", "application/json");
 
-			httpConn.setRequestMethod("POST");
+            httpConn.setRequestMethod("POST");
 
-			httpConn.setRequestProperty("Authorization", "Basic " + encodedAuthorization);
+            httpConn.setRequestProperty("Authorization", "Basic " + encodedAuthorization);
 
-			httpConn.setDoOutput(true);
-			out = httpConn.getOutputStream();
-			String activityJsonStr = new JSONObject(activity).toString();
+            httpConn.setDoOutput(true);
+            out = httpConn.getOutputStream();
+            String activityJsonStr = new JSONObject(activity).toString();
 
-			out.write(activityJsonStr.getBytes());
-			out.flush();
+            out.write(activityJsonStr.getBytes("UTF-8"));
+            out.flush();
 
-			if (httpConn.getResponseCode() >= 400) {
-				if (httpConn != null) {
-					InputStreamReader errStream = new InputStreamReader(httpConn.getErrorStream());
-					BufferedReader br = new BufferedReader(errStream);
-					String read = br.readLine();
+            if (httpConn.getResponseCode() >= 400) {
+                if (httpConn != null) {
+                    InputStreamReader errStream = new InputStreamReader(httpConn.getErrorStream());
+                    BufferedReader br = new BufferedReader(errStream);
+                    String read = br.readLine();
 
-					while (read != null) {
-						System.out.println(read);
-						read = br.readLine();
-					}
-				}
+                    while (read != null) {
+                        System.out.println(read);
+                        read = br.readLine();
+                    }
+                }
 
-				throw new Exception("HTTP ERROR:" + httpConn.getResponseCode());
-			}
+                throw new Exception("HTTP ERROR:" + httpConn.getResponseCode());
+            }
 
-		} finally {
-			closeOutputStream(out);
-			// endTime = System.currentTimeMillis();
-			// System.out.println(Thread.currentThread().getName()+
-			// "end to close output stream" + (endTime-startTime)/1000);
+        } finally {
+            closeOutputStream(out);
+            // endTime = System.currentTimeMillis();
+            // System.out.println(Thread.currentThread().getName()+
+            // "end to close output stream" + (endTime-startTime)/1000);
 
-			// System.out.println(Thread.currentThread().getName()+
-			// "begin to close connection");
-			// startTime = System.currentTimeMillis();
-			closeHttpConnection(httpConn);
-		}
+            // System.out.println(Thread.currentThread().getName()+
+            // "begin to close connection");
+            // startTime = System.currentTimeMillis();
+            closeHttpConnection(httpConn);
+        }
 
-	}
+    }
 
-	private void closeInputStream(InputStream is) {
-		try {
-			if (is != null) {
-				is.close();
-			}
-		} catch (Exception ex) {
-		}
+    private void closeInputStream(InputStream is) {
+        try {
+            if (is != null) {
+                is.close();
+            }
+        } catch (Exception ex) {
+        }
 
-	}
+    }
 
-	private void closeHttpConnection(HttpURLConnection httpConn) {
-		try {
-			if (httpConn != null) {
-				httpConn.disconnect();
-			}
-		} catch (Exception ex) {
-		}
+    private void closeHttpConnection(HttpURLConnection httpConn) {
+        try {
+            if (httpConn != null) {
+                httpConn.disconnect();
+            }
+        } catch (Exception ex) {
+        }
 
-	}
+    }
 
-	private void closeOutputStream(OutputStream out) {
-		try {
-			if (out != null) {
-				out.close();
-			}
-		} catch (Exception ex) {
-		}
+    private void closeOutputStream(OutputStream out) {
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (Exception ex) {
+        }
 
-	}
-	
-	public static void main(String[] args) {
-		Activity activity = new Activity();
-		activity.setId(100);
-		activity.setText2("ABC");
-		JSONObject obj = new JSONObject(activity);
-		System.out.println(obj.toString());
-	}
+    }
+
+    public static void main(String[] args) {
+        Activity activity = new Activity();
+        activity.setId(100);
+        activity.setText2("ABC");
+        JSONObject obj = new JSONObject(activity);
+        System.out.println(obj.toString());
+    }
 }
